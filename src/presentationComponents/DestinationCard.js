f3ee6010583e684831387fe323f71218b7e8315e
Damien <dsaulnier@jahia.com>
2018-03-16 18:01:47 +0100

    Avoid the destination card to crash on missing photo

--- src/presentationComponents/DestinationCard.js
+++ src/presentationComponents/DestinationCard.js
@@ -1,40 +1,31 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+
 /* import '../styles/DestinationCard.css';  TODO review this */
 
 class DestinationCard extends Component {
     render() {
         const destination = this.props.destination;
-        const photoUrls = destination.photo;
-        const name = destination.name;
-        const country= destination.country;
-
-        // TODO review this
-        //if(destination && name && country && photoUrls && photoUrls.length > 0) {
-        if(destination && name) {
-            // TODO review this (hardcoded DX hostname)
-            const photo = "http://localhost:8080/files/live" + photoUrls.files[0].path
-            return (
-                <div className = "destination-card">
-                    <img className = "destination-photo" src = {photo} />
-                    <div className = "destination-name">{name}</div>
-                    <div className = "destination-country">{country}</div>
-                    <Link to = {`/destination/${destination.id}`}>Destination Link test</Link>
-                </div>
-            )
-            /*
-            return (
-                <div className = "destination-card">
-                    <img className = "destination-photo" src = {photoUrls[0]} />
-                    <div className = "destination-name">{name}</div>
-                    <div className = "destination-country">{country}</div>
-                    <Link to = {`/destination/${destination.name}`}>Destination Link test</Link>
-                </div>
-            )
-            */
-        } else {
+        if (!destination) {
             console.log("The destination object isn't correctly set");
+            return
         }
+        const photoNode = destination.photo.files[0];
+        const name = destination.name;
+        const country = destination.country;
+
+        // TODO review this (hardcoded DX hostname)
+        const photo = photoNode ?
+            "http://localhost:8080/files/live" + photoNode.path
+            : null;
+        return (
+            <div className="destination-card">
+                <img className="destination-photo" src={photo}/>
+                <div className="destination-name">{name}</div>
+                <div className="destination-country">{country}</div>
+                <Link to={`/destination/${destination.id}`}>Destination Link test</Link>
+            </div>
+        )
     }
 }
 
