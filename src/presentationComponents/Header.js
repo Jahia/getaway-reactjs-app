982f11a5f3bed2468615abba2ea440d2c46649ca
Damien <dsaulnier@jahia.com>
2018-03-16 18:09:39 +0100

    Allow to go back to home by clicking the logo

--- src/presentationComponents/Header.js
+++ src/presentationComponents/Header.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import '../styles/Header.css';
+import {Link} from "react-router-dom";
 
 class Header extends Component {
     render() {
         return (
             <nav className = "headerMain">
                 <div className = "logo-container">
-                    <img src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/346954/getaway.svg" alt = "GetAway logo" />
+                    <Link to={`/`}>
+                        <img src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/346954/getaway.svg" alt = "GetAway logo" />
+                    </Link>
                 </div>
             </nav>
         )
