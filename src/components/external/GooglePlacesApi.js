4667b1ec3938b5d7c8ce90329a682831bd551e24
vandre <vandre@jahia.com>
2018-04-11 15:48:21 +0200

    Avoid initialization Google Places service several times / remove Google Maps container from index.html and create it on the fly

--- src/components/external/GooglePlacesApi.js
+++ src/components/external/GooglePlacesApi.js
@@ -3,23 +3,30 @@ const GOOGLE_MAP_CONTAINER_ID = 'google_map_container';
 
 class GooglePlacesApi {
 
+    constructor() {
+        this.placesService = null;
+    }
     /**
      * Call and store the given landmark's details
      */
     getPlaceDetails(placeId) {
         const googleMaps = window.google.maps;
         const request = this.buildGetLandmarkRQ(placeId);
-        const placesService = this.initGooglePlacesService();
 
-        return new Promise(function(resolve, reject) {
-            placesService.getDetails(request, function(place, status) {
-                if(status == googleMaps.places.PlacesServiceStatus.OK) {
-                    resolve(place);
-                } else {
-                    reject(status);
-                }
+        const placesService = this.getGooglePlacesService();
+        if(placesService) {
+            return new Promise(function(resolve, reject) {
+                placesService.getDetails(request, function(place, status) {
+                    if(status == googleMaps.places.PlacesServiceStatus.OK) {
+                        resolve(place);
+                    } else {
+                        reject(status);
+                    }
+                });
             });
-        });
+        } else {
+            console.log("The Google Places service is not initialized correctly");
+        }
     }
 
     /**
@@ -34,14 +41,27 @@ class GooglePlacesApi {
     }
 
     /**
-     * Initializes the Google Maps service
+     * Retrieve the Google Places service or Initializes it
+     * @return {Object} - The initialized Google Places service
+     */
+    getGooglePlacesService() {
+        if(!this.placesService) {
+            this.initGooglePlacesService();
+        }
+
+        return this.placesService;
+    }
+
+    /**
+     * Initializes the Google Places service
      * @return {Object} - The initialized Google Maps service
      */
     initGooglePlacesService() {
         const googleMaps = window.google.maps;
-        const mapContainer = document.getElementById(GOOGLE_MAP_CONTAINER_ID);
+        // as long as the service is not initialize in a React component accessing the DOM will work
+        const mapContainer = document.createElement("google_map_container");
         const map = new googleMaps.Map(mapContainer);
-        return new googleMaps.places.PlacesService(map);
+        this.placesService = new googleMaps.places.PlacesService(map);
     }
 }
 
