47e38a9f4b0431bc4d71f45b9237aa82cc545040
vandre <vandre@jahia.com>
2018-04-11 10:45:47 +0200

    put GooglePlacesApiProvider at the root

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import registerServiceWorker from './registerServiceWorker';
 import GetawayConstants from './utils/GetawayConstants'
 import {ApolloProvider} from 'react-apollo'
 import {ApolloClient} from 'apollo-client'
-import {ApolloLink} from 'apollo-client-preset'
 import {HttpLink} from 'apollo-link-http'
 import {InMemoryCache} from 'apollo-cache-inmemory'
+import GoogleApi from "./components/external/GooglePlacesApi";
+import GooglePlacesApiProvider from "./components/external/GooglePlacesApiProvider";
 
 const httpLink = new HttpLink({
     uri: GetawayConstants.dxHost + '/modules/graphql'
@@ -20,12 +21,15 @@ const client = new ApolloClient({
     cache: new InMemoryCache()
 });
 
+const placesApi = new GoogleApi();
 
 ReactDOM.render(
     <BrowserRouter>
-        <ApolloProvider client={client}>
-            <App/>
-        </ApolloProvider>
+        <GooglePlacesApiProvider placesApi={placesApi}>
+            <ApolloProvider client={client}>
+                <App/>
+            </ApolloProvider>
+        </GooglePlacesApiProvider>
     </BrowserRouter>,
     document.getElementById('root'));
 registerServiceWorker();
